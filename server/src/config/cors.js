c24925f192f1e48c7d4f88719ba241e3a08de604
QuasimKhan <quasimkhanpersonal@gmail.com>
2025-11-27 00:39:20 +0530

    updated

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -2,6 +2,6 @@ import cors from "cors";
 import { ENV } from "./env.js";
 
 export const corsOptions = {
-    origin: ENV.CLIENT_URL,
+    origin: ENV.CLIENT_URL || "http://localhost:5000",
     credentials: true,
 };
